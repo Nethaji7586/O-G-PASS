dc0c0b0e18920e4200174eccd2852b6c94990606
Nethaji.v <155124384+Nethaji7586@users.noreply.github.com>
2026-01-31 13:14:48 +0530

    Delete backend directory

--- backend/middleware/staffOnly.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (req, res, next) {
-  if (req.user.role !== "staff") {
-    return res.status(403).json({ msg: "Staff access only" });
-  }
-  next();
-};
--- backend/middleware/studentOnly.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (req, res, next) {
-  if (req.user.role !== "student") {
-    return res.status(403).json({ msg: "Student access only" });
-  }
-  next();
-};
