1a0014c6be4bbd6fe1f3e56c59db0ea43c01e5e9
Nethaji.v <155124384+Nethaji7586@users.noreply.github.com>
2026-02-04 14:49:57 +0530

    Delete lib directory

--- lib/config/api_config.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-class ApiConfig {
-  // Base URL depending on environment
-  static String get baseUrl {
-    // If .env has BASE_URL, use it; otherwise default to Render production URL
-    return dotenv.env['BASE_URL'] ?? "https://o-g-pass.onrender.com/api";
-  }
-}
